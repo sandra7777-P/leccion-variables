a31c8a673a696e65315efbecc40e58620e1ffe9e
Sandra Piza <131202421+sandra7777-P@users.noreply.github.com>
2023-05-31 11:33:02 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+var age =  parseInt(prompt("¿cual es tu edad?")); //42
+//convirtiendo edad a segundos
+var ageInSeconds = age*365*24*60*60;
+//mostrando edad en la web
+document.write("Tu edad es "+ age + " y en segundos seria " + ageInSeconds);
